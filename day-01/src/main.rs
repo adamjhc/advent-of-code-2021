7c31374a3c3c36f82d53527fe2431543032e8ba6
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-12-01 10:12:15 +0000

    Complete Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,6 +2,7 @@ use utils;
 
 fn main() {
     println!("{}", part_1());
+    println!("{}", part_2());
 }
 
 fn part_1() -> u32 {
@@ -23,6 +24,25 @@ fn part_1() -> u32 {
     increases
 }
 
+fn part_2() -> u32 {
+    let depths: Vec<u32> = utils::read_lines("./input.txt")
+        .map(|depth_str| depth_str.unwrap().parse().unwrap())
+        .collect();
+
+    let mut increases = 0u32;
+    let mut previous_depth = u32::MAX;
+    for i in 2..depths.len() {
+        let depth_sliding_window = depths[i] + depths[i - 1] + depths[i - 2];
+        if depth_sliding_window > previous_depth {
+            increases += 1;
+        }
+
+        previous_depth = depth_sliding_window;
+    }
+
+    increases
+}
+
 #[cfg(test)]
 mod day_01_tests {
     use super::*;
@@ -30,5 +50,10 @@ mod day_01_tests {
     #[test]
     fn part_1_gives_correct_answer() {
         assert_eq!(part_1(), 1233)
+    }
+
+    #[test]
+    fn part_2_gives_correct_answer() {
+        assert_eq!(part_2(), 1275)
     }
 }
