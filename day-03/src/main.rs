5f8fa49afc2b561a9b7a2ea372138face9a41fd6
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-12-06 15:27:17 +0000

    Not working Part 2 solution

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,7 +1,8 @@
 use utils;
 
 fn main() {
-    println!("{}", part_one())
+    println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -29,6 +30,81 @@ fn part_one() -> usize {
     let epsilon_rate = gamma_rate ^ 0b1111_1111_1111;
 
     gamma_rate * epsilon_rate
+}
+
+fn part_two() -> u32 {
+    let input = utils::read_string("./input.txt");
+    let half_length = input.lines().count() as u32 / 2;
+
+    let binary_digits: Vec<Vec<u32>> = input
+        .lines()
+        .map(|binary_string| {
+            binary_string
+                .chars()
+                .map(|binary_digit| binary_digit.to_digit(10).unwrap())
+                .collect()
+        })
+        .collect();
+
+    let mut ogr_considerations = binary_digits.clone();
+    let mut index = 0;
+    loop {
+        let sum = ogr_considerations
+            .iter()
+            .fold(0, |sum, binary| sum + binary[index]);
+
+        let most_common = if sum >= half_length { 1 } else { 0 };
+
+        ogr_considerations = ogr_considerations
+            .iter()
+            .filter(|&binary| binary[index] == most_common)
+            .map(Vec::to_owned)
+            .collect();
+
+        if ogr_considerations.len() == 1 {
+            break;
+        }
+
+        index += 1;
+    }
+
+    let mut csr_considerations = binary_digits.clone();
+    let mut index = 0;
+    loop {
+        let sum = csr_considerations
+            .iter()
+            .fold(0, |sum, binary| sum + binary[index]);
+
+        let most_common = if sum >= half_length { 0 } else { 1 };
+
+        csr_considerations = csr_considerations
+            .iter()
+            .filter(|&binary| binary[index] == most_common)
+            .map(Vec::to_owned)
+            .collect();
+
+        if csr_considerations.len() == 1 {
+            break;
+        }
+
+        index += 1;
+    }
+
+    let ogr: u32 = ogr_considerations[0]
+        .iter()
+        .fold(0, |mut binary, binary_digit| {
+            binary <<= 1;
+            binary + binary_digit
+        });
+
+    let csr: u32 = csr_considerations[0]
+        .iter()
+        .fold(0, |mut binary, binary_digit| {
+            binary <<= 1;
+            binary + binary_digit
+        });
+
+    ogr * csr
 }
 
 #[cfg(test)]
