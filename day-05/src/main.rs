26b2c6dfa2ffa5a0b0994f2d3adb7e053e78adfa
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-12-08 10:39:54 +0000

    Complete part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -13,13 +13,26 @@ impl Line {
     fn new(from: Point, to: Point) -> Line {
         let mut segments = Vec::new();
 
-        let min_x = from.x.min(to.x);
-        let min_y = from.y.min(to.y);
-        let max_x = from.x.max(to.x);
-        let max_y = from.y.max(to.y);
+        let range_x: Vec<usize> = if from.x < to.x {
+            (from.x..=to.x).collect()
+        } else {
+            (to.x..=from.x).rev().collect()
+        };
 
-        for x in min_x..=max_x {
-            for y in min_y..=max_y {
+        let range_y: Vec<usize> = if from.y < to.y {
+            (from.y..=to.y).collect()
+        } else {
+            (to.y..=from.y).rev().collect()
+        };
+
+        if from.y == to.y || from.x == to.x {
+            for x in range_x {
+                for y in &range_y {
+                    segments.push(Point { x, y: *y })
+                }
+            }
+        } else {
+            for (&x, y) in range_x.iter().zip(range_y) {
                 segments.push(Point { x, y })
             }
         }
@@ -32,14 +45,15 @@ impl Line {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 struct Point {
     x: usize,
     y: usize,
 }
 
 fn main() {
-    println!("{}", part_one())
+    println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -49,6 +63,23 @@ fn part_one() -> usize {
 
     let mut points: HashMap<Point, usize> = HashMap::new();
     orthogonal_lines.iter().for_each(|&line| {
+        line.segments.iter().for_each(|&point| {
+            if points.contains_key(&point) {
+                *points.get_mut(&point).unwrap() += 1;
+            } else {
+                points.insert(point, 1);
+            }
+        })
+    });
+
+    points.iter().filter(|(_, &count)| count >= 2).count()
+}
+
+fn part_two() -> usize {
+    let lines = parse_input();
+
+    let mut points: HashMap<Point, usize> = HashMap::new();
+    lines.iter().for_each(|line| {
         line.segments.iter().for_each(|&point| {
             if points.contains_key(&point) {
                 *points.get_mut(&point).unwrap() += 1;
@@ -91,5 +122,138 @@ mod day_05_tests {
     #[test]
     fn part_one_gives_correct_answer() {
         assert_eq!(part_one(), 8350)
+    }
+
+    #[test]
+    fn part_two_gives_correct_answer() {
+        assert_eq!(part_two(), 19374)
+    }
+
+    #[test]
+    fn line_segments_horizontal_to_right() {
+        let line = Line::new(Point { x: 0, y: 3 }, Point { x: 5, y: 3 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 0, y: 3 },
+                Point { x: 1, y: 3 },
+                Point { x: 2, y: 3 },
+                Point { x: 3, y: 3 },
+                Point { x: 4, y: 3 },
+                Point { x: 5, y: 3 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_horizontal_to_left() {
+        let line = Line::new(Point { x: 5, y: 3 }, Point { x: 0, y: 3 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 5, y: 3 },
+                Point { x: 4, y: 3 },
+                Point { x: 3, y: 3 },
+                Point { x: 2, y: 3 },
+                Point { x: 1, y: 3 },
+                Point { x: 0, y: 3 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_vertical_up() {
+        let line = Line::new(Point { x: 3, y: 0 }, Point { x: 3, y: 5 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 3, y: 0 },
+                Point { x: 3, y: 1 },
+                Point { x: 3, y: 2 },
+                Point { x: 3, y: 3 },
+                Point { x: 3, y: 4 },
+                Point { x: 3, y: 5 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_vertical_down() {
+        let line = Line::new(Point { x: 3, y: 5 }, Point { x: 3, y: 0 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 3, y: 5 },
+                Point { x: 3, y: 4 },
+                Point { x: 3, y: 3 },
+                Point { x: 3, y: 2 },
+                Point { x: 3, y: 1 },
+                Point { x: 3, y: 0 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_diagonal_up_right() {
+        let line = Line::new(Point { x: 0, y: 3 }, Point { x: 3, y: 6 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 0, y: 3 },
+                Point { x: 1, y: 4 },
+                Point { x: 2, y: 5 },
+                Point { x: 3, y: 6 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_diagonal_down_right() {
+        let line = Line::new(Point { x: 0, y: 6 }, Point { x: 3, y: 3 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 0, y: 6 },
+                Point { x: 1, y: 5 },
+                Point { x: 2, y: 4 },
+                Point { x: 3, y: 3 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_diagonal_up_left() {
+        let line = Line::new(Point { x: 3, y: 3 }, Point { x: 0, y: 6 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 3, y: 3 },
+                Point { x: 2, y: 4 },
+                Point { x: 1, y: 5 },
+                Point { x: 0, y: 6 },
+            ]
+        )
+    }
+
+    #[test]
+    fn line_segments_diagonal_down_left() {
+        let line = Line::new(Point { x: 3, y: 6 }, Point { x: 0, y: 3 });
+
+        assert_eq!(
+            line.segments,
+            vec![
+                Point { x: 3, y: 6 },
+                Point { x: 2, y: 5 },
+                Point { x: 1, y: 4 },
+                Point { x: 0, y: 3 },
+            ]
+        )
     }
 }
