2db25d52621c0130997b1cb3eab4570734b53779
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-12-08 15:24:49 +0000

    Complete part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,7 +1,8 @@
 use utils;
 
 fn main() {
-    println!("{}", part_one())
+    println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -20,6 +21,18 @@ fn part_one() -> usize {
     })
 }
 
+fn part_two() -> usize {
+    let crab_positions = parse_input();
+
+    let mean = crab_positions.iter().sum::<usize>() / crab_positions.len();
+
+    crab_positions.iter().fold(0, |fuel, &pos| {
+        let steps = if pos > mean { pos - mean } else { mean - pos };
+
+        fuel + (steps * (steps + 1) / 2)
+    })
+}
+
 fn parse_input() -> Vec<usize> {
     utils::read_string("./input.txt")
         .split(",")
@@ -34,5 +47,10 @@ mod day_07_tests {
     #[test]
     fn part_one_gives_correct_answer() {
         assert_eq!(part_one(), 344735)
+    }
+
+    #[test]
+    fn part_two_gives_correct_answer() {
+        assert_eq!(part_two(), 96798233)
     }
 }
