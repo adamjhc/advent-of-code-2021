b841c6cd313012799c53e4e26b780daaf5c5068d
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-12-08 15:13:51 +0000

    Complete day 7 part 1

---
+++ day-07/src/main.rs
@@ -0,0 +1,38 @@
+use utils;
+
+fn main() {
+    println!("{}", part_one())
+}
+
+fn part_one() -> usize {
+    let mut crab_positions = parse_input();
+
+    crab_positions.sort();
+
+    let median = crab_positions[crab_positions.len() / 2];
+
+    crab_positions.iter().fold(0, |fuel, &pos| {
+        fuel + if pos > median {
+            pos - median
+        } else {
+            median - pos
+        }
+    })
+}
+
+fn parse_input() -> Vec<usize> {
+    utils::read_string("./input.txt")
+        .split(",")
+        .filter_map(|num| num.parse().ok())
+        .collect()
+}
+
+#[cfg(test)]
+mod day_07_tests {
+    use super::*;
+
+    #[test]
+    fn part_one_gives_correct_answer() {
+        assert_eq!(part_one(), 344735)
+    }
+}
