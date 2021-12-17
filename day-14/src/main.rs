b994a9fbc586e7cbccce95f9015e0d1456b9d2c1
Adam Cox <adamjhcox@gmail.com>
2021-12-17 10:45:06 +0000

    Add slightly broken part 2 solution

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -3,7 +3,8 @@ use std::collections::HashMap;
 use utils;
 
 fn main() {
-    println!("{}", part_one())
+    println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -32,6 +33,67 @@ fn part_one() -> usize {
         .chars()
         .fold(HashMap::new(), |mut counts, character| {
             *counts.entry(character).or_insert(0) += 1;
+            counts
+        });
+
+    let (high, low) = counts
+        .iter()
+        .fold((0, usize::MAX), |(high, low), (_, &count)| {
+            if count > high {
+                (count, low)
+            } else if count < low {
+                (high, count)
+            } else {
+                (high, low)
+            }
+        });
+
+    high - low
+}
+
+fn part_two() -> usize {
+    let (polymer_template, insertion_rules) = parse_input();
+
+    let pairs = polymer_template.chars().collect::<Vec<char>>();
+    let mut counts_of_pairs = pairs.windows(2).fold(HashMap::new(), |mut counts, pair| {
+        *counts.entry((pair[0], pair[1])).or_insert(0) += 1;
+        counts
+    });
+
+    let insertion_rules: HashMap<(char, char), char> = insertion_rules
+        .iter()
+        .map(|(key, value)| {
+            let characters: Vec<char> = key.chars().collect();
+
+            (
+                (characters[0], characters[1]),
+                value.chars().next().unwrap(),
+            )
+        })
+        .into_iter()
+        .collect();
+
+    for _ in 0..40 {
+        let mut count_of_pairs_new = HashMap::new();
+
+        for (pair, count) in counts_of_pairs {
+            if let Some(&insertion) = insertion_rules.get(&pair) {
+                *count_of_pairs_new.entry((pair.0, insertion)).or_insert(0) += count;
+                *count_of_pairs_new.entry((insertion, pair.1)).or_insert(0) += count;
+            } else {
+                *count_of_pairs_new.entry(pair).or_insert(0) += count;
+            }
+        }
+
+        counts_of_pairs = count_of_pairs_new;
+    }
+
+    // this will count characters in overlapping pairs twice
+    let counts = counts_of_pairs
+        .iter()
+        .fold(HashMap::new(), |mut counts, (characters, count)| {
+            *counts.entry(characters.0).or_insert(0) += count;
+            *counts.entry(characters.1).or_insert(0) += count;
             counts
         });
 
